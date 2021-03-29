80e4a90bd67dff831252a34da91bcf51f0499194
Oscar Lindqvist <olindqv@gmail.com>
2021-03-29 17:29:24 +0200

    fix image urls

--- src/database/mock/mockItems.js
+++ src/database/mock/mockItems.js
@@ -4,21 +4,25 @@ const Item = require('../schemas/item');
 const mockObjects = [{
   itemTitle: 'desk',
   itemDescription: 'a very beautiful desk',
-  itemImages: ['https://unsplash.com/photos/3d4sSUChunA'],
+  itemImages: ['https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1536&q=80'],
   itemCategory: 'furniture',
   itemOwner: 'Mina',
 },
 {
   itemTitle: 'Harry Potter',
   itemDescription: 'old version of Harry Potter',
-  itemImages: ['https://unsplash.com/photos/CXYPfveiuis', 'https://unsplash.com/photos/xY55bL5mZAM'],
+  itemImages: [
+    'https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', 
+    'https://images.unsplash.com/photo-1512820790803-83ca734da794?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1374&q=80'],
   itemCategory: 'books',
   itemOwner: 'Oscar',
 },
 {
   itemTitle: 'jacket',
   itemDescription: 'nice jacket',
-  itemImages: ['https://unsplash.com/photos/opJElA_rBNw'],
+  itemImages: [
+    'https://images.unsplash.com/photo-1592878904946-b3cd8ae243d0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=801&q=80'
+  ],
   itemCategory: 'clothes',
   itemOwner: 'Tom',
 }];
