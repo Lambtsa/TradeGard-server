4a5edd72b8aaeae87c3920aa1172afdd9290bc6c
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-03-31 11:51:27 +0200

    add users to okta

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { createUser } = require('../../database/services/users-service');
+const fetch = require('node-fetch');
 
 router.post('/', async (req, res, next) => {
   if (!req.body.user) {
@@ -10,10 +11,41 @@ router.post('/', async (req, res, next) => {
   try {
     const newUser = req.body.user;
     await createUser(newUser.userDisplayName);
+    const oktaUser = {
+      profile: {
+        firstName: newUser.userFirstName,
+        lastName: newUser.userLastName,
+        email: newUser.userEmail,
+        login: newUser.userEmail,
+        mobilePhone: newUser.userTelephone,
+        nickName: newUser.userDisplayName,
+      },
+      groupIds: [
+        process.env.OKTA_GROUP_ID,
+      ],
+      credentials: {
+        password: {
+          value: newUser.userPassword,
+        }
+      }
+    }
     // post to okta
+    const response = await fetch(process.env.OKTA_API_PATH, {
+      method: 'POST',
+      headers: { 
+        'Content-type': 'application/json',
+        Authorization: `SSWS ${process.env.OKTA_API_TOKEN}`
+      },
+      body: JSON.stringify(oktaUser),
+    });
+    const data = await response.json();
+    console.log(data);
+    res
+      .status(201)
+      .send('resource created');
   } catch (err) {
-
+    next(err);
   }
-  
-
 });
+
+module.exports = router;
