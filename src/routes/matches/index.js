436ef75a37a06cd4b8e5c759652f6cbd0f89f656
Lambtsa <42414095+Lambtsa@users.noreply.github.com>
2021-04-05 14:24:39 +0200

    contact details for matches

--- src/routes/matches/index.js
+++ src/routes/matches/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { authenticationRequired } = require('../../authentication/authentication-service');
-const { getUserLikes } = require('../../database/services/users-service');
+const { getUserLikes, getContactDetails } = require('../../database/services/users-service');
 const { getItemById, getItemsByUserId } = require('../../database/services/items-service');
 
 router.get('/', authenticationRequired, async (req, res, next) => {
@@ -29,8 +29,18 @@ router.get('/', authenticationRequired, async (req, res, next) => {
         matched: userMatch,
       }
     }));
-    console.log(authenticatedUserMatches);
-    res.json(authenticatedUserMatches);
+    const filteredUserMatches = authenticatedUserMatches.filter(match => match.matched);
+    const matchesWithDetails = await Promise.all(filteredUserMatches.map(async userMatch => {
+      const response = await getContactDetails(userMatch.id);
+      const contactDetails = await response.json();
+      const matchObj = {
+        userDisplayName: contactDetails.profile.nickName,
+        userEmail: contactDetails.profile.email,
+        userTelephone: contactDetails.profile.mobilePhone ? contactDetails.profile.mobilePhone : '',
+      };
+      return matchObj;
+    }));
+    res.json(matchesWithDetails);
   } catch (err) {
     err.statusCode = 400;
     next(err);
