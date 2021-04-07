eb4ee1a9639e0ec516a05f6004ab2118ec4cd178
Mina Hosseini <minaa.hosseinii@gmail.com>
2021-04-07 11:27:26 +0200

    finish my account page

--- src/database/schemas/item.js
+++ src/database/schemas/item.js
@@ -20,7 +20,7 @@ const itemSchema = new mongoose.Schema({
       'phones',
       'services',
       'tools',
-      'vehicules',
+      'vehicles',
     ],
     required: [true, 'The item category is required'],
   },
--- src/routes/items/index.js
+++ src/routes/items/index.js
@@ -54,6 +54,7 @@ router.get('/:id', authenticateUser, async (req, res, next) => {
       itemDescription: item.itemDescription,
       itemTitle: item.itemTitle,
       itemOwner: {
+        userId: contactDetails.id,
         userDisplayName: contactDetails.profile.nickName,
         userEmail: contactDetails.profile.email,
         userTelephone: contactDetails.profile.mobilePhone ? contactDetails.profile.mobilePhone : '',
